49d6be5f9146f6cf1398d5558ba0aa1faf7d64b6
Setu Potnis <spotnis.work@gmail.com>
2024-11-03 14:14:45 -0800

    feat: update error handling for products API

--- src/server/services/ProductService.ts
+++ src/server/services/ProductService.ts
@@ -1,44 +1,81 @@
 import { IProduct } from "../models/Product";
 import { productRepository } from "../repositories/ProductRepository";
+import { SortOrder } from "mongoose";
+import logger from "../logger";
 
 export const productService = {
     async retrieveAllProducts(
         sortQuery: string,
         searchString: string
-    ): Promise<IProduct[]> {
-        const sort: Record<string, number> = {}; // default to no sorting
-        if (sortQuery) {
-            const [key, order] = sortQuery.split(":");
-            sort[key] = order === "asc" ? 1 : -1; // MongoDB uses 1 for ascending, -1 for descending
+    ): Promise<IProduct[] | undefined> {
+        try {
+            const sort: { [key: string]: SortOrder } = {}; // default to no sorting
+            if (sortQuery) {
+                const [key, order] = sortQuery.split(":");
+                sort[key] = order === "asc" ? 1 : -1; // MongoDB uses 1 for ascending, -1 for descending
+            }
+            if (!searchString) {
+                return productRepository.retrieveAllProducts(sort);
+            }
+            return productRepository.retrieveAllProducts(sort, searchString);
+        } catch (error) {
+            if (error instanceof Error) {
+                const errorMessage = `Service error. Something went wrong retrieving products; ${error.message}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
+            }
         }
-        if (!searchString) {
-            return productRepository.retrieveAllProducts(sort);
-        }
-        return productRepository.retrieveAllProducts(sort, searchString);
     },
     async createProduct(
-        name: string,
-        description: string,
-        price: number,
-        category: string,
-        stock: number
-    ): Promise<IProduct> {
-        const newProduct = await productRepository.createProduct({
-            name,
-            description,
-            price,
-            category,
-            stock,
-        });
-        return newProduct;
+        name: string | undefined,
+        description: string | undefined,
+        price: number | undefined,
+        category: string | undefined,
+        stock: number | undefined
+    ): Promise<IProduct | undefined> {
+        if (!name || !description || !price || !category || !stock) {
+            logger.error("Service error. Required params are not provided");
+            throw new Error("Service error. Required params are not provided");
+        }
+        try {
+            return await productRepository.createProduct({
+                name,
+                description,
+                price,
+                category,
+                stock,
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                const errorMessage = `Service error. ${error.message}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
+            }
+        }
     },
     async updateProduct(
         productId: string,
         productData: Partial<IProduct>
-    ): Promise<IProduct | null> {
-        return productRepository.updateProduct(productId, productData);
+    ): Promise<IProduct | undefined | null> {
+        try {
+            return productRepository.updateProduct(productId, productData);
+        } catch (error) {
+            if (error instanceof Error) {
+                const errorMessage = `Service error. ${error.message}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
+            }
+        }
     },
-    async deleteProduct(productId: string): Promise<IProduct | null> {
-        return productRepository.deleteProduct(productId);
+    async deleteProduct(productId: string): Promise<IProduct | undefined | null> {
+        try {
+            return productRepository.deleteProduct(productId);
+        } catch (error) {
+            if (error instanceof Error) {
+                const errorMessage = `Service error. ${error.message}`;
+                logger.error(errorMessage);
+                throw new Error(errorMessage);
+            }
+        }
     },
 };
